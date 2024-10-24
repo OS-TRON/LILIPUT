a038a48bd6eeddec436970ed95e7550e7b1eed7b
OS-TRON <lehaleskortov@gmail.com>
2024-10-24 21:40:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const axios =require("axios");
+const axios = require("axios");
 const express = require("express");
 const app = express();
 const token = process.env.TELEGRAM_BOT_TOKEN;
@@ -9,27 +9,33 @@ const webAppUrl = 'https://bugalteria.netlify.app/';
 app.get("/", (req, res) => {
     res.send("Bot в шоке");
 });
-const port = 8000;
+const port = 3000;
 app.listen(port, () => {
-    console.log('Server running at http://localhost:${port}');
-})
+    console.log(`Server running at http://localhost:${port}`);
+});
 
-const bot  = new TelegramBot( token, {polling: true});
+const bot = new TelegramBot(token, {polling: true});
+
+// Обработка ошибки polling_error
+bot.on('polling_error', (error) => {
+    console.error('Polling error:', error.code, error.message);
+});
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    if(text === '/start') {
-        await bot.sendMessage(chatId, 'Добро пожаловать! ', {
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Добро пожаловать!', {
             reply_markup: {
                 inline_keyboard: [
                     [{text: 'Войти', web_app: {url: webAppUrl}}]
                 ]
             }
         });
-    }else {return};
-
+    } else {
+        return;
+    }
 });
 
 module.exports = app;
